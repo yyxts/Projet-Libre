558c98f45fa52b7a5baa99da8840cd47f42980c5
Unknown <zhangbolun2356@gmail.com>
2020-03-15 16:18:24 +0100

    Create Activity for Remind and Medicine

---
+++ app/src/main/java/univ/polytech/projetlibre/Activity/AddRemindActivity.java
@@ -0,0 +1,15 @@
+package univ.polytech.projetlibre.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import univ.polytech.projetlibre.R;
+
+public class AddRemindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_remind);
+    }
+}
---
+++ app/src/main/java/univ/polytech/projetlibre/Activity/MedicineListActivity.java
@@ -0,0 +1,16 @@
+package univ.polytech.projetlibre.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import univ.polytech.projetlibre.R;
+
+public class MedicineListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_list);
+    }
+
+}
